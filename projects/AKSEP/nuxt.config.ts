06dd2f896db5ced1a16ed5e26fc3bfc217c1ce1b
Trummler <132260271+Trummler12@users.noreply.github.com>
2025-08-25 15:08:36 +0200

    AKSEP: fix project setup

--- projects/AKSEP/nuxt.config.ts
+++ projects/AKSEP/nuxt.config.ts
@@ -1,3 +1,14 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', '@nuxtjs/i18n']
+  modules: ['@nuxt/content', '@nuxtjs/i18n'],
+  srcDir: 'src',
+  nitro: {
+    compatibilityDate: '2025-08-25'
+  },
+  i18n: {
+    vueI18n: './i18n.config.ts',
+    locales: ['de', 'en'],
+    defaultLocale: 'de'
+  }
 })
---
+++ projects/AKSEP/tests/nuxt-config.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest'
+import config from '../nuxt.config'
+
+describe('nuxt configuration', () => {
+  it('uses src directory', () => {
+    expect(config.srcDir).toBe('src')
+  })
+})
