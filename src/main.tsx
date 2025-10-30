1b4e65c99f2772f34aa9292d2519016ff33ed3da
Trummler <132260271+Trummler12@users.noreply.github.com>
2025-10-30 22:01:19 +0100

    move index.css to styles and rename the file to indicate that I don't like that abomination

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './styles/.figma-ui-components.css';
 
 const container = document.getElementById('root');
 
